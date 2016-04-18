8d8ae875babe5fa9cc0ce2112da11d5d206aba6d
X-Force <xtremforce@users.noreply.github.com>
2016-04-19 00:10:46 +0800

    Update RemoveAds.js
    Added pixabay.com

--- RemoveAds.js
+++ RemoveAds.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name       Remove Ads by X-Force
 // @namespace   http://www.iplaysoft.com/
-// @version    0.131
-// @description  Remove Gmail Ads
+// @version    0.14
+// @description  Remove Website Ads
 // @match      https://mail.google.com/mail/*
 // @match      http://weibo.com/*
 // @match      http://www.cnbeta.com/*
+// @match      https://pixabay.com/*
 // @downloadURL https://raw.githubusercontent.com/xtremforce/UserScripts/master/RemoveAds.js
 // @updateURL https://raw.githubusercontent.com/xtremforce/UserScripts/master/RemoveAds.js
 // @grant GM_addStyle
@@ -46,5 +47,9 @@
     if(window.location.href.indexOf('//www.cnbeta.com/')>2){
         GM_addStyle('div.mt5 div iframe,div.tal,div.cbv,div.navi,#googleAd_afc,#BAIDU_DUP_wrapper_u1240009_0,#job_box,#fixed_area,#tanxssp_con_mm_10007624_104555_13330262,#aswift_2_expand,#BAIDU_DUP_wrapper_945055_0,#BAIDU_DUP_wrapper_u1309802_0 {display:none;}');
         return;
+    }
+    
+    if(window.location.href.indexOf('//pixabay.com/')>2){
+        GM_addStyle(".open_preview_img { visibility:hidden; }");
     }
 })();
