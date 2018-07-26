a04968c544dd628bbb9b3530a2577901790914f2
X-Force <xtremforce@users.noreply.github.com>
2018-07-26 21:03:22 +0800

    Update open_in_itunes.js
    不对 Mac App Store 生效

--- open_in_itunes.js
+++ open_in_itunes.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         找回「在 iTunes 中查看」按钮
+// @name         Bring back Open in iTunes Button
 // @namespace    https://www.iplaysoft.com
-// @version      0.31
-// @description  帮你找回「在 iTunes 中查看」按钮
+// @version      0.32
+// @description  Bring back [Open in iTunes] Button. 帮你找回「在 iTunes 中查看」按钮
 // @author       X-Force
 // @match        https://itunes.apple.com/*
 // @grant        none
@@ -11,19 +11,21 @@
 // ==/UserScript==
 
 (function() {
-    var url = location.href.split('#')[0];
-    var regex = /\/id([0-9]+)/;
-    var match = url.match(regex);
-    var id = null;
-    if(match!==null){
-        id = match[1];
-    }
-    if(id!==null){
-        var xurl = "itmss://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id="+id+"&mt=8&at=10laHZ";
-        var mydiv = document.createElement("div");
-        var html = '<style>#xf_itunes_link{display: inline-block;padding: 8px 22px;background: #228fff;color: #fff;font-size: 16px;border-radius: 6px;}#xf_itunes_link:hover{text-decoration:none}</style>';
-        html = html + '<a id="xf_itunes_link" href="'+xurl+'">在 iTunes 中查看</a>';
-        mydiv.innerHTML = html;
-        document.getElementsByClassName("product-header")[0].appendChild(mydiv);
+    if(document.title.match("Mac App Store")==null){
+        var url = location.href.split('#')[0];
+        var regex = /\/id([0-9]+)/;
+        var match = url.match(regex);
+        var id = null;
+        if(match!==null){
+            id = match[1];
+        }
+        if(id!==null){
+            var xurl = "itmss://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id="+id+"&mt=8&at=10laHZ";
+            var mydiv = document.createElement("div");
+            var html = '<style>#xf_itunes_link{display: inline-block;padding: 8px 22px;background: #228fff;color: #fff;font-size: 16px;border-radius: 6px;}#xf_itunes_link:hover{text-decoration:none}</style>';
+            html = html + '<a id="xf_itunes_link" href="'+xurl+'">在 iTunes 中查看</a>';
+            mydiv.innerHTML = html;
+            document.getElementsByClassName("product-header")[0].appendChild(mydiv);
+        }
     }
 })();
